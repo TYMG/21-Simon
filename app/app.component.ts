464d6f43ec1c05b06792a77f8f1a30fab7881e41
Matthew Green <verde.mateo.a@gmail.com>
2016-11-09 00:26:46 -0500

    gameboard css

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: '<h1>My Second Angular App</h1>'
+  templateUrl: "game-board.html"
 })
 export class AppComponent { }
